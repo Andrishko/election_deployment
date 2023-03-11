8d53581074460c6019c9fa512ed69794e34f7da7
Andrishko <andrewpetrovckiy@gmail.com>
2023-03-11 20:36:11 +0200

    last version

--- election/settings.py
+++ election/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "election.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'zeek2002',  # пароль
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
